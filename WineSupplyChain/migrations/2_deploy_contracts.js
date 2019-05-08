9c5f97a07d40aa3dca38dd73223ed349519fa18f
Paulo H. C. Oliveros <paulohco@gmail.com>
2019-05-07 21:24:18 -0300

    Removing WineSuppyChain from this repository

--- WineSupplyChain/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var WineChainAccessControl = artifacts.require("./WineChainAccessControl.sol");
-var WineSupplyChain = artifacts.require("./WineSupplyChain.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(WineChainAccessControl);
-    deployer.deploy(WineSupplyChain, 'Vinicola Oliveros', 2020);
-};
-
